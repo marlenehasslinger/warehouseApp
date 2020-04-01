7677d2619ec75cefc9a66bf8fe9bc171d465aaab
Marlene Hasslinger <hasslingermarlene@gmail.com>
2020-04-01 14:38:26 -0400

    add search functionality for drivers and trucks

--- src/app/pages/trucklog/trucklog.page.ts
+++ src/app/pages/trucklog/trucklog.page.ts
@@ -2,6 +2,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
 import { TruckService, truck } from 'src/app/services/truck.service';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-trucklog',
@@ -10,8 +11,12 @@ import { TruckService, truck } from 'src/app/services/truck.service';
 })
 export class TrucklogPage implements OnInit {
   trucks: truck[];
+  searchClicked: boolean = false;
+  //search results
+  sampleArr = [];
+  resultArr = [];
 
-  constructor(private truckService: TruckService, private router: Router) { }
+  constructor(private truckService: TruckService, private router: Router, public fs: AngularFirestore) { }
 
   ngOnInit() {
     this.truckService.getAllTrucks().subscribe(res => {
@@ -20,11 +25,55 @@ export class TrucklogPage implements OnInit {
     });
   }
 
-  navigateToTruckOverview(truck: truck){
-    console.log("uid from trucklog: "+ truck.id);
+  navigateToTruckOverview(truck: truck) {
+    console.log("uid from trucklog: " + truck.id);
     this.router.navigate(['/menu/trucklog/truckoverview/', truck.id]);
 
   }
-  
+
+
+  search(event) {
+    console.log("entered search function");
+    let searchKey: string = event.target.value;
+    console.log("searchKey: " + searchKey);
+    let firstLetter = searchKey.toUpperCase();
+    console.log("firstLetter: " + firstLetter);
+
+    if (searchKey.length == 0) {
+      this.searchClicked = false;
+      this.sampleArr = [];
+      this.resultArr = [];
+    } else {
+      this.searchClicked = true;
+    }
+
+    if (this.sampleArr.length == 0) {
+      console.log("in sampleArr.length==0");
+      this.fs.collection('trucks', ref => ref.where('searchIndex', '==', firstLetter)).snapshotChanges()
+        .subscribe(data => {
+          data.forEach(childData => {
+            console.log("childData: " + JSON.stringify(childData.payload.doc.data()));
+            this.sampleArr.push(childData.payload.doc.data());
+            this.resultArr.push(childData.payload.doc.data());
+            console.log("sampleArr: " + JSON.stringify(this.sampleArr));
+          })
+        })
+    }
+    console.log("in sampleArr.length!=0");
+    this.resultArr = [];
+    this.sampleArr.forEach(val => {
+      let name: string = val['name'];
+      let n: string = name.toString().toUpperCase();
+      if (n.startsWith(searchKey.toUpperCase())) {
+        if (true) {
+          console.log("in true loop");
+          this.resultArr.push(val);
+          console.log("resultArr: " + JSON.stringify(this.resultArr));
+        }
+      }
+    })
+
+  }
+
 
 }
--- src/app/pages/userlog/userlog.page.ts
+++ src/app/pages/userlog/userlog.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService, user } from 'src/app/services/user.service';
 import { Router } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-userlog',
@@ -9,8 +10,13 @@ import { Router } from '@angular/router';
 })
 export class UserlogPage implements OnInit {
   users: user[];
+  searchClicked:boolean = false;
 
-  constructor(private userService: UserService, private router: Router) { }
+  //search results
+  sampleArr=[];
+  resultArr=[];
+
+  constructor(private userService: UserService, private router: Router, public fs: AngularFirestore) { }
 
   ngOnInit() {
     this.userService.getAllUsers().subscribe(res => {
@@ -24,6 +30,49 @@ export class UserlogPage implements OnInit {
     //this.router.navigate(['userdetail', JSON.stringify(user)]);
     this.router.navigate(['/menu/userlog/userlogdetail/', user.uid]);
 
+  }
+
+  search(event){
+    console.log("entered search function");
+    let searchKey:string=event.target.value;
+    console.log("searchKey: " + searchKey);
+    let firstLetter=searchKey.toUpperCase();
+    console.log("firstLetter: " + firstLetter);
+
+    if(searchKey.length==0){
+      this.searchClicked = false;
+      this.sampleArr=[];
+      this.resultArr=[];
+    } else {
+      this.searchClicked = true;
+    }
+
+    if(this.sampleArr.length==0){
+      console.log("in sampleArr.length==0");
+      this.fs.collection('users', ref => ref.where('searchIndex', '==', firstLetter)).snapshotChanges()
+        .subscribe(data => {
+          data.forEach(childData => {
+            console.log("childData: " + JSON.stringify(childData.payload.doc.data()));
+            this.sampleArr.push(childData.payload.doc.data());
+            this.resultArr.push(childData.payload.doc.data());
+            console.log("sampleArr: " + JSON.stringify(this.sampleArr));
+          })
+        })
+    } 
+      console.log("in sampleArr.length!=0");
+      this.resultArr=[];
+      this.sampleArr.forEach(val=>{
+        let name:string=val['firstname'];
+        let n:string = name.toString().toUpperCase();
+        if(n.startsWith(searchKey.toUpperCase())){
+          if(true){
+            console.log("in true loop");
+            this.resultArr.push(val);
+            console.log("resultArr: " + JSON.stringify(this.resultArr));
+          }
+        }
+      })
+    
   }
 
 }
